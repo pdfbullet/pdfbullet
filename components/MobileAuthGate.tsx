9bc9dabedb726871812729f2d66d847294c767ed
ashutosh sahh <ashutosh@pdfbullet.com>
2026-07-14 17:41:09 +0545

    Disable MobileAuthGate to allow direct guest access on mobile

--- components/MobileAuthGate.tsx
+++ components/MobileAuthGate.tsx
@@ -1,35 +1,11 @@
 import React from 'react';
-import { useAuth } from '../contexts/AuthContext.tsx';
-import Preloader from './Preloader.tsx';
-import MobileAuthScreen from './MobileAuthScreen.tsx';
 
 interface MobileAuthGateProps {
     children: React.ReactNode;
     onOpenForgotPasswordModal: () => void;
 }
 
-const MobileAuthGate: React.FC<MobileAuthGateProps> = ({ children, onOpenForgotPasswordModal }) => {
-    const { user, loading } = useAuth();
-    const [isGuest, setIsGuest] = React.useState(() => {
-        return localStorage.getItem('guest_mode') === 'true';
-    });
-
-    if (loading) {
-        return <Preloader />;
-    }
-
-    if (!user && !isGuest) {
-        return (
-            <MobileAuthScreen 
-                onOpenForgotPasswordModal={onOpenForgotPasswordModal} 
-                onContinueAsGuest={() => {
-                    localStorage.setItem('guest_mode', 'true');
-                    setIsGuest(true);
-                }} 
-            />
-        );
-    }
-
+const MobileAuthGate: React.FC<MobileAuthGateProps> = ({ children }) => {
     return <>{children}</>;
 };
 
