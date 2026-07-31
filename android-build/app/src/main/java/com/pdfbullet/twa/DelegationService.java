39628a3a40411b0a251627c856784bfa78554b13
ashutosh sahh <ashutosh@pdfbullet.com>
2026-07-31 20:54:04 +0545

    chore: save clean android-build configuration targeting API 36 (version code 4)

---
+++ android-build/app/src/main/java/com/pdfbullet/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.pdfbullet.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
